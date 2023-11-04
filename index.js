0aa6c2d0fe5a0c8c86cf7e0e2c701fa2eac98688
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-11-04 13:55:24 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs").promises;
+const argv = require("yargs").argv;
+
+const { listContacts, getContactById, removeContact, addContact} = require("./contacts.js");
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id)
+      break;
+
+    case "add":
+    addContact(name, email, phone)
+      break;
+
+    case "remove":
+      removeContact(id)
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
